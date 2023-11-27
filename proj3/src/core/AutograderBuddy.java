934079281f815a7486c523144088cdbef284667a
Justin <justincho321@gmail.com>
2023-11-27 03:24:28 -0800

    final 3.1

--- proj3/src/core/AutograderBuddy.java
+++ proj3/src/core/AutograderBuddy.java
@@ -118,7 +118,7 @@ public class AutograderBuddy {
                 String aPos = world.getAPos()[0] + "," + world.getAPos()[1];
                 try {
                     FileWriter myWriter =
-                            new FileWriter("file:///C:\\Programs\\CS61B\\fa23-proj3-g232\\proj3\\saveGame.txt");
+                            new FileWriter("file://C:\\Programs\\CS61B\\fa23-proj3-g232\\proj3\\saveGame.txt");
                     myWriter.write(lastSeed + "\n"); //first line seed
                     myWriter.write(aPos + "\n"); //second line avatar position
                     myWriter.write(lightsOff + "\n"); //third line lights on/off
@@ -167,7 +167,7 @@ public class AutograderBuddy {
 
     public TETile[][] autoLoadGame() {
 
-        In in = new In("file:///C:\\Programs\\CS61B\\fa23-proj3-g232\\proj3\\saveGame.txt");
+        In in = new In("file://C:\\Programs\\CS61B\\fa23-proj3-g232\\proj3\\saveGame.txt");
         String strSeed = in.readLine();
         String positionStr = in.readLine();
         String strAx = positionStr.split(",")[0];
